9a8e6a2554928e60baca9a6488a39e7b881ea914
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-04-02 12:15:16 +0200

    App

---
+++ sound-pads/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import pads from "./assets/pads.js";
+
+export default function App() {
+  const [padState, setPadState] = React.useState(pads);
+
+  const buttonElements = padState.map((pad) => <button className="button-class" key={pad.id}></button>);
+
+  return (
+    <main>
+      <div className="pad-container">{buttonElements}</div>
+    </main>
+  );
+}
