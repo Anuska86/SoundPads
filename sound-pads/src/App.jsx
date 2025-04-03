4393f1728347c930a4e88bd12369db8bbd8ce82a
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-04-03 12:50:04 +0200

    Update App.jsx

--- sound-pads/src/App.jsx
+++ sound-pads/src/App.jsx
@@ -5,9 +5,26 @@ import Pad from "./components/Pad.jsx";
 export default function App() {
   const [padsState, setPadsState] = React.useState(pads);
 
+  function toggle(id) {
+    console.log("clicked!!");
+    setPadsState((prevPads) =>
+      prevPads.map((pad) =>
+        pad.id === id ? { ...pad, on: !pad.on } : pad
+      )
+    );
+  }
+
   const buttonElements = padsState.map((pad) => {
-    return <Pad key={pad.id} color={pad.color} on={pad.on} />;
+    return (
+      <Pad
+        key={pad.id}
+        color={pad.color}
+        on={pad.on}
+        toggle={() => toggle(pad.id)}
+      />
+    );
   });
+
   return (
     <main>
       <div className="pad-container">{buttonElements}</div>
